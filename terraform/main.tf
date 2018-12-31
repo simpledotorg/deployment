ec234ba467703dd1dbdef9f2abae0b7b4357008a
Govind Krishna Joshi <govind@nilenso.com>
2018-12-31 12:24:01 +0530

    Import existing QA server

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,30 +2,24 @@
 # Set up the AWS SSH key pair
 #
 resource "aws_key_pair" "redapp-staging" {
-  key_name   = "redapp-staging"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
+  key_name   = "redapp-server-staging"
+  public_key = "${file("~/.ssh/redapp-server-staging.pub")}"
 }
 
 #
 # Provision the EC2 instances
 #
-resource "aws_instance" "redapp-staging" {
-  ami = "${data.aws_ami.ubuntu.id}"
+resource "aws_instance" "qa_simple_server" {
+  ami = "${var.qa_ami}"
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.redapp-staging.key_name}"
 
   root_block_device {
-    volume_size = "100"
+    volume_size = "8"
   }
 
-  security_groups = [
-    "${aws_security_group.allow_ssh.name}",
-    "${aws_security_group.allow_http_https.name}",
-    "${aws_security_group.allow_outbound.name}"
-  ]
-
   tags {
-    Name = "redapp-staging"
+    Name = "simple-server-vpc2-qa"
   }
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,7 @@
 variable "aws_region" {
   default = "ap-south-1"
 }
+
+variable "qa_ami" {
+  default = "ami-0279be4ad6b012061"
+}
