f0a7f09646d5d61edf6524d9c79ce27711cd9952
Govind Krishna Joshi <govind@nilenso.com>
2018-12-31 15:25:01 +0530

    Add output for all environments

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,3 +21,15 @@ resource "aws_key_pair" "simple_aws_key" {
 output "qa-simple-server-ip" {
   value = "${aws_eip.qa_simple_server.public_ip}"
 }
+
+output "staging-simple-server-ip" {
+  value = "${aws_eip.staging_simple_server.public_ip}"
+}
+
+output "production-simple-server-ip" {
+  value = "${aws_eip.production_simple_server.public_ip}"
+}
+
+output "cardreader-simple-server-ip" {
+  value = "${aws_eip.production_cardreader.public_ip}"
+}
