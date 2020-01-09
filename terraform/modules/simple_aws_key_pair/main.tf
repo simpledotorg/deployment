ca69b1188f9f5617b11cfbb5c23749afc17d3812
Govind Krishna Joshi <govind@nilenso.com>
2020-01-09 07:23:06 +0530

    Split networking into smaller files

--- terraform/modules/simple_aws_key_pair/main.tf
+++ terraform/modules/simple_aws_key_pair/main.tf
@@ -4,6 +4,6 @@ resource "aws_key_pair" "simple_aws_key" {
 }
 
 output "simple_aws_key_name" {
-	value = aws_key_pair.simple_aws_key.key_name
+	value       = aws_key_pair.simple_aws_key.key_name
 	description = "Name of the default key pair"
 }
