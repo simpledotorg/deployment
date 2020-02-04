357bf5f8c4b476febe61310238f546e0a2809824
Govind Krishna Joshi <govind@nilenso.com>
2020-02-04 13:53:09 +0530

    Lock ami version to avoid accidental updates (#145)

--- terraform/modules/simple_server/aws_ami.tf
+++ terraform/modules/simple_server/aws_ami.tf
@@ -1,6 +1,4 @@
 data "aws_ami" "ubuntu" {
-  most_recent = true
-
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
@@ -9,6 +7,11 @@ data "aws_ami" "ubuntu" {
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
+  }
+
+  filter {
+    name = "image-id"
+    values = ["ami-0c28d7c6dd94fb3a7"]
   }
 
   owners = ["099720109477"] # Use official Canonical Ubuntu AMIs
