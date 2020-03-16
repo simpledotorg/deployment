cc14a46fe71ac9acbc76999fc5ef717534be7962
prabhanshuguptagit <prabhanshu98@gmail.com>
2020-03-16 13:22:53 +0530

    Revert "Recreate boxes"
    This reverts commit 3a8e1bf91ca1245695589fcb6263d9bf1360c243.

--- standalone/provision-playground/terraform/main.tf
+++ standalone/provision-playground/terraform/main.tf
@@ -26,7 +26,7 @@ provider "digitalocean" {
 # Create a new Web Droplet in the nyc2 region
 resource "digitalocean_droplet" "icmr-box" {
   image  = "ubuntu-16-04-x64"
-  count  = 8
+  count  = 7
   name   = "p-icmr-box-${count.index + 1}"
   region = "blr1"
   size   = "c-2"
