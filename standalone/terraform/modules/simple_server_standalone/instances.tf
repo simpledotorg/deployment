fc4c0738b5fff3debd74248a11c24f2bd1f1cb6b
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-08-13 13:19:42 +0530

    Indent

--- standalone/terraform/modules/simple_server_standalone/instances.tf
+++ standalone/terraform/modules/simple_server_standalone/instances.tf
@@ -1,10 +1,10 @@
 # Create a new Web Droplet in the blr1 region
 resource "digitalocean_droplet" "simple_digitalocean_instance" {
-  image  = "ubuntu-16-04-x64"
-  count  = var.server_count
-  name   = "${var.deployment_name}-box-${count.index + 1}"
-  region = "blr1"
-  size   = var.digitalocean_instance_type
+  image    = "ubuntu-16-04-x64"
+  count    = var.server_count
+  name     = "${var.deployment_name}-box-${count.index + 1}"
+  region   = "blr1"
+  size     = var.digitalocean_instance_type
   ssh_keys = var.ssh_keys
 }
 
