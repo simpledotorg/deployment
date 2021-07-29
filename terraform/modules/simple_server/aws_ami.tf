358989798e806a9359384d97d4173a74872a19cb
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-07-29 15:47:55 +0530

    Add comments about AMI ID

--- terraform/modules/simple_server/aws_ami.tf
+++ terraform/modules/simple_server/aws_ami.tf
@@ -1,7 +1,9 @@
 data "aws_ami" "ubuntu" {
+  //  ami-0c28d7c6dd94fb3a7 is Canonical Community Ubuntu 16.04 LTS
+  //  ami-0c1a7f89451184c8b is Canonical Community Ubuntu 20.04 LTS
   filter {
     name = "image-id"
-    values = var.ec2_ubuntu_version == "16.04" ? ["ami-0c28d7c6dd94fb3a7"] : ["ami-0c1a7f89451184c8b"]
+    values = var.ec2_ubuntu_version == "16.04" ? ["ami-0c28d7c6dd94fb3a7"] : ["ami-0c1a7f89451184c8b"] # Defaults to Ubuntu 20.04
   }
 
   owners = ["099720109477"] # Use official Canonical Ubuntu AMIs
