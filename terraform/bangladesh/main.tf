61d0769ba2a5d35ab70c696955e2b7284dd19bf6
Prabhanshu Gupta <guptaprabhanshu1@gmail.com>
2021-07-29 16:13:08 +0530

    Move to Ubuntu 20.04 (#355)

--- terraform/bangladesh/main.tf
+++ terraform/bangladesh/main.tf
@@ -133,6 +133,7 @@ module "simple_server_bangladesh_production" {
   database_vpc_id               = module.simple_networking.database_vpc_id
   database_subnet_group_name    = module.simple_networking.database_subnet_group_name
   ec2_instance_type             = "t2.medium"
+  ec2_ubuntu_version            = "16.04"
   database_username             = var.bangladesh_database_username
   database_password             = var.bangladesh_database_password
   instance_security_groups      = module.simple_networking.instance_security_groups
@@ -157,6 +158,7 @@ module "simple_server_bangladesh_staging" {
   database_vpc_id               = module.simple_networking.database_vpc_id
   database_subnet_group_name    = module.simple_networking.database_subnet_group_name
   ec2_instance_type             = "t2.medium"
+  ec2_ubuntu_version            = "16.04"
   database_username             = var.bangladesh_staging_database_username
   database_password             = var.bangladesh_staging_database_password
   instance_security_groups      = module.simple_networking.instance_security_groups
--- terraform/modules/simple_server/aws_ami.tf
+++ terraform/modules/simple_server/aws_ami.tf
@@ -1,17 +1,9 @@
 data "aws_ami" "ubuntu" {
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
+  //  ami-0c28d7c6dd94fb3a7 is Canonical Community Ubuntu 16.04 LTS
+  //  ami-0c1a7f89451184c8b is Canonical Community Ubuntu 20.04 LTS
   filter {
     name = "image-id"
-    values = ["ami-0c28d7c6dd94fb3a7"]
+    values = var.ec2_ubuntu_version == "16.04" ? ["ami-0c28d7c6dd94fb3a7"] : ["ami-0c1a7f89451184c8b"] # Defaults to Ubuntu 20.04
   }
 
   owners = ["099720109477"] # Use official Canonical Ubuntu AMIs
