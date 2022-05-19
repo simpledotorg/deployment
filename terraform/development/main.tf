28b85b1d8718d4d9c0b68098fbb367e68ab7f99a
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2022-05-19 17:37:26 +0530

    Output load balancer public DNS

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -16,7 +16,7 @@ terraform {
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.9.0"
     }
   }
@@ -81,18 +81,18 @@ variable "certificate_private_key_file" {
 # slack credentials for cloudwatch
 #
 variable "slack_webhook_url" {
-	description = "Slack webhook URL for creating AWS SNS topic"
-	type        = string
+  description = "Slack webhook URL for creating AWS SNS topic"
+  type        = string
 }
 
 variable "slack_channel" {
-	description = "Slack channel name to which notifications are sent"
-	type        = string
+  description = "Slack channel name to which notifications are sent"
+  type        = string
 }
 
 variable "slack_username" {
-	description = "Slack username to user for sending notifications"
-	type        = string
+  description = "Slack username to user for sending notifications"
+  type        = string
 }
 
 #
@@ -106,14 +106,14 @@ module "simple_aws_key_pair" {
 # redis
 #
 module "simple_redis_param_group" {
-  source            = "../modules/simple_redis_param_group"
+  source = "../modules/simple_redis_param_group"
 }
 
 #
 # networking
 #
 module "simple_networking" {
-  source            = "../modules/simple_networking"
+  source = "../modules/simple_networking"
 
   deployment_name   = "development"
   database_vpc_cidr = "172.32.0.0/16"
@@ -129,10 +129,10 @@ module "notify_slack" {
   source  = "terraform-aws-modules/notify-slack/aws"
   version = "5.0.0"
 
-  sns_topic_name       = "cloudwatch-to-slack"
-  slack_webhook_url    = var.slack_webhook_url
-  slack_channel        = var.slack_channel
-  slack_username       = var.slack_username
+  sns_topic_name    = "cloudwatch-to-slack"
+  slack_webhook_url = var.slack_webhook_url
+  slack_channel     = var.slack_channel
+  slack_username    = var.slack_username
 
   lambda_function_name = "cloudwatch-to-slack"
 }
@@ -185,6 +185,9 @@ output "simple_server_sandbox_sidekiq_redis_url" {
   value = module.simple_server_sandbox.sidekiq_redis_url
 }
 
+output "simple_server_sandbox_load_balancer_public_dns" {
+  value = module.simple_networking.load_balancer_public_dns
+}
 
 module "simple_server_qa" {
   source                        = "../modules/simple_server"
@@ -226,6 +229,10 @@ output "simple_server_qa_cache_redis_url" {
 
 output "simple_server_qa_sidekiq_redis_url" {
   value = module.simple_server_qa.sidekiq_redis_url
+}
+
+output "simple_server_qa_load_balancer_public_dns" {
+  value = module.simple_networking.load_balancer_public_dns
 }
 
 
@@ -271,6 +278,11 @@ output "simple_server_security_sidekiq_redis_url" {
   value = module.simple_server_security.sidekiq_redis_url
 }
 
+output "simple_server_security_load_balancer_public_dns" {
+  value = module.simple_networking.load_balancer_public_dns
+}
+
+
 
 /* This sets up a bunch of ec2 servers for the standalone setup
    with the appropriate security groups.
@@ -291,3 +303,4 @@ module "simple_server_playground" {
   monitoring_server_count    = 1
   storage_server_count       = 1
 } */
+
