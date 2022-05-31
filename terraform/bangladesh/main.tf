09cbaaaa10bea78f17e04e25379735390e88618a
Prabhanshu Gupta <guptaprabhanshu1@gmail.com>
2022-05-31 22:52:57 +0530

    Upgrade BD postgres (#446)

--- terraform/bangladesh/main.tf
+++ terraform/bangladesh/main.tf
@@ -135,28 +135,31 @@ module "notify_slack" {
 # server configs
 #
 module "simple_server_bangladesh_production" {
-  source                        = "../modules/simple_server"
-  deployment_name               = "bangladesh-production"
-  database_vpc_id               = module.simple_networking.database_vpc_id
-  database_subnet_group_name    = module.simple_networking.database_subnet_group_name
-  ec2_instance_type             = "t3.xlarge"
-  ec2_ubuntu_version            = "20.04"
-  database_username             = var.bangladesh_database_username
-  database_password             = var.bangladesh_database_password
-  instance_security_groups      = module.simple_networking.instance_security_groups
-  aws_key_name                  = module.simple_aws_key_pair.simple_aws_key_name
-  server_vpc_id                 = module.simple_networking.server_vpc_id
-  https_listener_arn            = module.simple_networking.https_listener_arn
-  load_balancer_arn_suffix      = module.simple_networking.load_balancer_arn_suffix
-  host_urls                     = ["bd.simple.org", "api.bd.simple.org", "dashboard.bd.simple.org"]
-  create_redis_cache_instance   = true
-  create_redis_sidekiq_instance = true
-  create_database_replica       = true
-  server_count                  = 2
-  sidekiq_server_count          = 1
-  redis_param_group_name        = module.simple_redis_param_group.redis_param_group_name
-  enable_cloudwatch_alerts      = true
-  cloudwatch_alerts_sns_arn     = module.notify_slack.this_slack_topic_arn
+  source                         = "../modules/simple_server"
+  deployment_name                = "bangladesh-production"
+  database_vpc_id                = module.simple_networking.database_vpc_id
+  database_subnet_group_name     = module.simple_networking.database_subnet_group_name
+  database_postgres_version      = "14.2"
+  database_instance_type         = "db.t3.medium"
+  database_replica_instance_type = "db.t3.medium"
+  ec2_instance_type              = "t3.xlarge"
+  ec2_ubuntu_version             = "20.04"
+  database_username              = var.bangladesh_database_username
+  database_password              = var.bangladesh_database_password
+  instance_security_groups       = module.simple_networking.instance_security_groups
+  aws_key_name                   = module.simple_aws_key_pair.simple_aws_key_name
+  server_vpc_id                  = module.simple_networking.server_vpc_id
+  https_listener_arn             = module.simple_networking.https_listener_arn
+  load_balancer_arn_suffix       = module.simple_networking.load_balancer_arn_suffix
+  host_urls                      = ["bd.simple.org", "api.bd.simple.org", "dashboard.bd.simple.org"]
+  create_redis_cache_instance    = true
+  create_redis_sidekiq_instance  = true
+  create_database_replica        = true
+  server_count                   = 2
+  sidekiq_server_count           = 1
+  redis_param_group_name         = module.simple_redis_param_group.redis_param_group_name
+  enable_cloudwatch_alerts       = true
+  cloudwatch_alerts_sns_arn      = module.notify_slack.this_slack_topic_arn
 }
 
 output "simple_server_bangladesh_production_server_instance_ips" {
