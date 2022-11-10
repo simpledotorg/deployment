0931a7315f22561f1e90eee64b5c34bc6b94326b
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2022-11-10 18:44:25 +0530

    Check for 2 consecutive failures before alerting

--- ansible/roles/cphc-migration/templates/cphc_connection_check.sh
+++ ansible/roles/cphc-migration/templates/cphc_connection_check.sh
@@ -1,12 +1,19 @@
 #! /usr/bin/bash
 
-naclient status;
-if [ $? -eq 1 ];
-then echo VPN is connected;
-else
-  naclient login -profile {{ cphc_vpn.profile }} -user {{ cphc_vpn.username }} -password '{{ cphc_vpn.password }}' <<< y
-  if [ $? -eq 1 ];
-    then echo Sucessfully connected to VPN;
-    else curl -X POST -H 'Content-type: application/json' --data '{"text":"Unable to connect CPHC VPN"}' {{ cphc_vpn.alerts_webhook_url }};
+CONSECUTIVE_FAILURES=0
+while true; do
+  naclient status
+  if [ $? -eq 1 ]; then
+    echo "$(date '+TIME:%H:%M:%S') VPN is connected"
+  else
+    CONSECUTIVE_FAILURES=$((CONSECUTIVE_FAILURES + 1))
+    naclient login -profile {{ cphc_vpn.profile }} -user {{ cphc_vpn.username }} -password '{{ cphc_vpn.password }}' <<< y
+    if [ $? -eq 1 ]; then
+      echo "$(date '+TIME:%H:%M:%S') Successfully connected to VPN"
+      CONSECUTIVE_FAILURES=0
+    elif [ $CONSECUTIVE_FAILURES -gt 2 ]; then
+      curl -X POST -H 'Content-type: application/json' --data '{"text":"Unable to connect CPHC VPN"}' {{ cphc_vpn.alerts_webhook_url }}
+    fi
   fi
-fi
+  sleep 5
+done
