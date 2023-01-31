4705df00ae8c5d237a975acfab3550e38bcecb64
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2023-01-31 14:01:27 +0530

    Add a security rule for S3 buckets

--- terraform/modules/simple_s3/main.tf
+++ terraform/modules/simple_s3/main.tf
@@ -39,3 +39,11 @@ output "user_secret_key" {
   sensitive = true
 }
 
+resource "aws_s3_bucket_public_access_block" "block_logs_public_access" {
+  bucket = aws_s3_bucket.logs_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
